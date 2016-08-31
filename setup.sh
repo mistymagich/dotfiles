703ebeeee289ef7ab1cb73c48143fb1fc2f2ad76
Masato Yamaguchi <m.yamaguchi@rabbitroom.jp>
2016-08-31 15:08:42 +0900

    gitconfigのuser.emailとuser.nameを初期設定時に設定するようにし、それをgit管理外の.gitconfig.localに保存するようにした

--- setup.sh
+++ setup.sh
@@ -41,3 +41,19 @@ if [ -e "$HOME/.ssh/authorized_keys" ]; then
 fi
 
 
+# ~/.gitconfig.local
+GIT_CONFIG_LOCAL=~/.gitconfig.local
+if [ ! -e $GIT_CONFIG_LOCAL ]; then
+	echo -n "git config user.email?> "
+	read GIT_AUTHOR_EMAIL
+
+	echo -n "git config user.name?> "
+	read GIT_AUTHOR_NAME
+
+	cat << EOF > $GIT_CONFIG_LOCAL
+[user]
+    name = $GIT_AUTHOR_NAME
+    email = $GIT_AUTHOR_EMAIL
+EOF
+fi
+
